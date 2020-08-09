7e6fa03cfd1efbd7a142e05b4edd6b36f01f8090
Jose Rodriguez <jose.rodriguez76@protonmail.com>
2020-08-09 12:03:59 -0700

    Add Interval and audio files

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 /*----- constants -----*/
+let sound = [
+    "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
+    "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
+    "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
+    "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3",
+    "https://s3.amazonaws.com/adam-recvlohe-sounds/error.wav"
+]
 
-const blueSound =        new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
-const greenSound =       new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
-const yellowSound =      new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
-const redSound =         new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-const gameOverSound =    new Audio("https://s3.amazonaws.com/adam-recvlohe-sounds/error.wav");
 
-const gameBoard = {
-    blue:   '1',
-    green:  '2',
-    yellow: '3',
-    red:    '4'
-}
 
 /*----- app's state (variables) -----*/
-let playerChoice = [];
-let computerChoice = [];
+let playerSeq = [];
+let computerSeq = [];
 let gameOver;
 let score;
+let interval;
+let color;
 
 /*----- cached element references -----*/
 document.querySelector('.blue')     .addEventListener('click', blueClicked);
@@ -33,22 +31,33 @@ document.querySelector('#resetBtn') .addEventListener('click', resetBtnClicked);
 
 function init() {
     gameOver = false;
+    score = 0;
     render();
 }
 
 function render() {
-    computerChoice = Math.floor(Math.random() * 4) + 1
-    
+    startBtnClicked();
 }
 
-
 function startBtnClicked (e) {
-
+    getRandomNumber();
+    let i = 0;
+    interval = setInterval(function () {
+        id = computerSeq[i];
+        color = $('#'+id).attr('class').split(' ')[1];
+    }, 1000);
 }
 function resetBtnClicked (e) {
     
 }
 
+function getRandomNumber () {
+    let randomNum = Math.floor(Math.random() * 4) + 1;
+    computerSeq.push(randomNum);
+}
+
+
+init();
 function blueClicked (e) {
     
 }
@@ -66,4 +75,3 @@ function redClicked (e) {
 
 }
 
-init();
