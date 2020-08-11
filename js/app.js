80d07d5107a02d86821929a1c220a12d1e4eafd5
Jose Rodriguez <jose.rodriguez76@protonmail.com>
2020-08-11 09:16:28 -0700

    Refactor functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*----- constants -----*/
-let sound = [
+const SOUND = [
     "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
     "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
     "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
@@ -7,15 +7,10 @@ let sound = [
     "https://s3.amazonaws.com/adam-recvlohe-sounds/error.wav"
 ]
 
-
-
 /*----- app's state (variables) -----*/
 let playerSeq = [];
 let computerSeq = [];
-let gameOver;
-let score;
-let interval;
-let color;
+let gameOver, score, color, msg;
 
 /*----- cached element references -----*/
 document.querySelector('.blue')     .addEventListener('click', blueClicked);
@@ -32,46 +27,87 @@ document.querySelector('#resetBtn') .addEventListener('click', resetBtnClicked);
 function init() {
     gameOver = false;
     score = 0;
+    startBtnClicked();
     render();
 }
 
 function render() {
-    startBtnClicked();
+    compareSequence();
+
 }
 
 function startBtnClicked (e) {
-    getRandomNumber();
-    let i = 0;
-    interval = setInterval(function () {
-        id = computerSeq[i];
-        color = $('#'+id).attr('class').split(' ')[1];
-    }, 1000);
+    getComputerSequence();
+    showCompSequence();
 }
+
 function resetBtnClicked (e) {
     
 }
 
-function getRandomNumber () {
+function getComputerSequence () {
     let randomNum = Math.floor(Math.random() * 4) + 1;
     computerSeq.push(randomNum);
 }
 
+function showCompSequence () {
+    let i = computerSeq.length - 1;
+    
+    id = computerSeq[i];
+    color = $('#' + id).attr('class').split(' ')[0];
+    // SOUND[i].play();
+    $('.' + color).addClass('flash');
+    
+    setTimeout(() => {
+        $('.' + color).removeClass('flash');
+    }, 250);
+}
+
+function compareSequence() {
+    if (computerSeq.toString() === playerSeq.toString()) {
+        score++;
+        getComputerSequence();
+        showCompSequence();
+    }
+}
 
 init();
+
+
 function blueClicked (e) {
-    
+    console.log(e.target)
+    playerSeq.push(1);
+    $('#1').addClass('flash');
+    setTimeout(() => {
+        $('#1').removeClass('flash');
+    }, 250);
 }
 
 function greenClicked (e) {
-    
+    console.log(e.target)
+    playerSeq.push(2);
+    $('#2').addClass('flash');
+    setTimeout(() => {
+        $('#2').removeClass('flash');
+    }, 250);  
 
 }
 function yellowClicked (e) {
-    
+    console.log(e.target)
+    playerSeq.push(3);
+    $('#3').addClass('flash');
+    setTimeout(() => {
+        $('#3').removeClass('flash');
+    }, 250);
 
 }
 function redClicked (e) {
-    
+    console.log(e.target)
+    playerSeq.push(4);
+    $('#4').addClass('flash');
+    setTimeout(() => {
+        $('#4').removeClass('flash');
+    }, 250);
 
 }
 
